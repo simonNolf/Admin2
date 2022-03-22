d1a9c49b801f03dc22a1b637a4197c260cc62942
severinRobert <sevrobe21@gmail.com>
2022-03-22 16:12:11 +0000

    Expose ports on docker run command insteadof in Dockerfile

--- scripts/rebuild_dns.sh
+++ scripts/rebuild_dns.sh
@@ -7,4 +7,4 @@ docker rmi dns
 printf "build new image\n"
 docker build -t dns ~/Admin2/DMZ/SOA-externe/
 printf "create new container\n"
-docker run -tid --name dnsCT dns
+docker run -tid -p 53:53/udp -p 53:53/tcp -p 127.0.0.1:953:953/tcp --name dnsCT dns
