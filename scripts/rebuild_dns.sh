f9c4d6688d7d0a8a75366609971ac41f00e41f8c
severinRobert <sevrobe21@gmail.com>
2022-03-22 12:40:42 +0000

    Add comment to dns scripts

--- scripts/rebuild_dns.sh
+++ scripts/rebuild_dns.sh
@@ -1,6 +1,10 @@
+printf "stop docker container\n"
 docker stop dnsCT
+printf "delete container and image\n"
 docker rm dnsCT
 docker rmi dns
 
+printf "build new image\n"
 docker build -t dns ~/Admin2/DMZ/SOA-externe/
+printf "create new container\n"
 docker run -tid --name dnsCT dns
