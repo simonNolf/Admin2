149a9920b2e36c313806da14139fc76386b21576
MagnesSimon <simon-mag@hotmail.com>
2022-05-28 17:50:41 +0200

    Config zone interne

---
+++ Trusted Zone/Services interne/intranet/index.php
@@ -0,0 +1,12 @@
+<!-- Page html de l'intranet -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <title>WoodyToys Intranet</title>
+</head>
+<body>
+   <p>Bienvenue sur l'intranet l2-2 de woody toy</p>
+</body>
